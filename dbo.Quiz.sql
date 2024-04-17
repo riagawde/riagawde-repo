faf20430a5998f43901e6fedc266d328e3a4d039
riagawde <167278552+riagawde@users.noreply.github.com>
2024-04-17 10:37:26 +0530

    Add files via upload

---
+++ dbo.Quiz.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Quiz] (
+    [Id]              INT           NOT NULL,
+    [Questions]       VARCHAR (MAX) NOT NULL,
+    [Answers]         VARCHAR (MAX) NOT NULL,
+    [correct options] NCHAR (10)    NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
